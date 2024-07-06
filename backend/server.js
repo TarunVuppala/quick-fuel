e79e52cbf92c6e9b34f9e3a9bb7c0d7706c55bc4
Tarun Vuppala <tarun.vuppala26@gmail.com>
2024-07-06 18:43:41 +0530

    setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const dotenv=require('dotenv');
 
 const app=express();
-
+dotenv.config();
 const PORT=process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>console.log('MongoDB connected'))
+    .catch(err=>console.log(err));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
